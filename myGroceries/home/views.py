bb1694486919e83ffe146fd23fa0866476377ba6
AlexanderKYu <alexanderkyu27@gmail.com>
2024-08-17 14:21:19 -0400

    feat: save new user data

--- myGroceries/home/views.py
+++ myGroceries/home/views.py
@@ -1,12 +1,13 @@
-from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.template import loader
+from user.models import User
 
 def home(request):
     if request.session.get('user_data', None):
-        fname = request.session['user_data']['given_name']
-        email = request.session['user_data']['email']
-        print(fname, email)
+        userFname = request.session['user_data']['given_name']
+        userEmail = request.session['user_data']['email']
+        if not User.objects.filter(email=userEmail):
+            user = User(fname = userFname, email = userEmail)
+            user.save()
         return render(request, 'home.html')
     else:
         return redirect('sim/')
