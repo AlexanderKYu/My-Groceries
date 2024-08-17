3f5f1236ec53fd32726662f99b3b7e9cc4b6d7b4
AlexanderKYu <alexanderkyu27@gmail.com>
2024-08-17 12:37:33 -0400

    feat: boiler plate for landing page in bootstrap

--- myGroceries/home/views.py
+++ myGroceries/home/views.py
@@ -4,6 +4,9 @@ from django.template import loader
 
 def home(request):
     if request.session.get('user_data', None):
+        fname = request.session['user_data']['given_name']
+        email = request.session['user_data']['email']
+        print(fname, email)
         return render(request, 'home.html')
     else:
         return redirect('sim/')
