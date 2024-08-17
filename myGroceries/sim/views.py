30a103d232520be8d576e01d6298dbb44e7d0102
AlexanderKYu <alexanderkyu27@gmail.com>
2024-08-17 11:23:58 -0400

    fix: fixed auth-reciver redirect to home

--- myGroceries/sim/views.py
+++ myGroceries/sim/views.py
@@ -30,7 +30,7 @@ def auth_receiver(request):
     # You could also authenticate the user here using the details from Google (https://docs.djangoproject.com/en/4.2/topics/auth/default/#how-to-log-a-user-in)
     request.session['user_data'] = user_data
 
-    return redirect('sign_in')
+    return redirect('/')
 
 
 def sign_out(request):
