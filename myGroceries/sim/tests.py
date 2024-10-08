55ba2eef7959c9a69385d70ae6e25ba0cdf91e3e
alexander yu <alexanderkyu27@gmail.com>
2024-10-07 22:52:16 -0400

    feat: add test suite for SIM

--- myGroceries/sim/tests.py
+++ myGroceries/sim/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.test import Client
 
-# Create your tests here.
+class signInModuleAuthorized(TestCase):
+    def setUp(self):
+        self.client = Client()
+        session = self.client.session
+        session['user_data'] = {'given_name': 'TestUser', 'email': 'test@user.com'}
+        session.save()
+
+    def test_OAuth_no_redirect(self):
+        with self.captureOnCommitCallbacks(execute=True) as callbacks:
+            response = self.client.get("/")
+        
+        self.assertEqual(response.status_code, 200)
+
