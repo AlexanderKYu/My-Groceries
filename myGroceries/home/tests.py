14467a58bec1157412298490e6ed6774eaceebaf
AlexanderKYu <alexanderkyu27@gmail.com>
2024-10-10 19:57:37 -0400

    feat: add tests for home page

--- myGroceries/home/tests.py
+++ myGroceries/home/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.test import Client
 
-# Create your tests here.
+class homeAuthorized(TestCase):
+    def setUp(self):
+        self.client = Client()
+        session = self.client.session
+        session['user_data'] = {'given_name': 'TestUser', 'email': 'test@user.com'}
+        session.save()
+    
+    def test_authorized_no_redirect(self):
+        response = self.client.get("/")
+
+        self.assertEqual(response.status_code, 200)
+
+class homeUnauthorized(TestCase):
+    def setUp(self):
+        self.client = Client()
+    
+    def test_unauthorized_redirect(self):
+        response = self.client.get("/")
+
+        self.assertRedirects(response, expected_url="/sim/", status_code=302)
