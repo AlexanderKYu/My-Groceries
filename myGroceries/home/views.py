81673103c66976d702f9d14a3dfc050278b32fac
alexander yu <alexanderkyu27@gmail.com>
2024-10-16 17:52:13 -0400

    feat: add email validation functionality to home page

--- myGroceries/home/views.py
+++ myGroceries/home/views.py
@@ -2,14 +2,19 @@ from django.shortcuts import render, redirect
 from user.models import User
 from groceries.models import Groceries
 from groceries.models import Tags
+from django.core.exceptions import ValidationError
 
 def home(request):
     if request.session.get('user_data', None):
         userFname = request.session['user_data']['given_name']
         userEmail = request.session['user_data']['email']
         if not User.objects.filter(email=userEmail):
-            user = User(fname = userFname, email = userEmail)
-            user.save()
+            try:
+                user = User(fname = userFname, email = userEmail)
+                user.save()
+            except ValidationError:
+                return redirect('/sim/')
+
         userId = User.objects.filter(email=userEmail).values('id')[0]['id']
         userInventory = Groceries.objects.filter(memberId=userId)
         context = {}
