06be1d274241435e1aa742c37c63cc93fe37213d
AlexanderKYu <alexanderkyu27@gmail.com>
2024-10-15 20:06:46 -0400

    feat: add user model tests with validation

--- myGroceries/user/models.py
+++ myGroceries/user/models.py
@@ -1,5 +1,6 @@
 from django.db import models
+from django.core.validators import validate_email
 
 class User(models.Model):
     fname = models.CharField(max_length=255)
-    email = models.EmailField()
+    email = models.EmailField(validators=[validate_email], unique=True)
--- myGroceries/user/tests.py
+++ myGroceries/user/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from user.models import User
+from django.core.exceptions import ValidationError
 
-# Create your tests here.
+class UserValidTests(TestCase):
+    def setUp(self):
+        User.objects.create(fname = "Test", email = "TestUser@example.com")
+
+    def test_valid_Entry_Creation(self):
+        testUser = User.objects.get(email="TestUser@example.com")
+        self.assertEqual(testUser.fname, "Test")
+
+class UserInvalidTests(TestCase):
+
+    def test_invalid_email(self):
+        invalidUser = User.objects.create(fname = "Test", email = "InvalidEmail")
+
+        self.assertRaises(ValidationError, invalidUser.full_clean)
